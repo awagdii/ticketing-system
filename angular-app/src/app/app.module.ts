a6dd0ec30665868060063cb4cb386bec0b58d23a
Ahmed Wagdi <ahmed.wagdi.moslem@gmail.com>
2019-06-16 21:31:26 -0500

    Update routing for Employees and Customers

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
 const myRoutes: Routes = [
-	{ path : '', component: AppComponent},
+	{ path : '', component: HomeComponent},
 	{ path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule)},
 	{ path: 'employees', loadChildren: () => import('./employees/employees.module').then(e => e.EmployeesModule)}
 ];
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		HomeComponent
 	],
 	imports: [
 		BrowserModule,RouterModule.forRoot(myRoutes)
--- angular-app/src/app/employees/employees.module.ts
+++ angular-app/src/app/employees/employees.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EmployeesComponent } from './employees.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [],
+  declarations: [EmployeesComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      { path: '', component: EmployeesComponent }
+    ])
   ]
 })
 export class EmployeesModule { }
